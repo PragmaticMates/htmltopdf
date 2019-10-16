ec86069aa65a0e2c3b40a1619f34e8d431db0bf2
rsoltys <radovan.soltys@gmail.com>
2019-10-16 12:54:44 +0200

    increase request timeout

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const saveToPdf = require("../controller/safeToPdf");
 
 router.post("/generate-pdf", async (req, res, __) => {
+    req.setTimeout(600000);
     let result = await saveToPdf(req.text);
     res.attachment('output.pdf');
     res.contentType("application/pdf");
