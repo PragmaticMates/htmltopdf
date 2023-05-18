0dc249be56ac6f5ff9a2d3596fea20cd3a6374f7
rsoltys <radovan.soltys@gmail.com>
2023-05-18 14:25:50 +0200

    speed up and show duration

--- controller/safeToPdf.js
+++ controller/safeToPdf.js
@@ -1,20 +1,49 @@
 const puppeteer = require("puppeteer-core");
 
-const saveToPdf = async htmlContent => {
+
+const browserHelper = (function() {
+  let instance; // Private variable to hold the single instance
+
+  async function createInstance() {
+    // Private method to create a new instance of the browser
+    const newInstance = await puppeteer.launch({
+      executablePath: "/usr/bin/chromium-browser",
+      args: ["--no-sandbox", "--disable-setuid-sandbox", "--run-all-compositor-stages-before-draw"] // SEE BELOW WARNING!!!
+    });
+    return newInstance;
+  }
+
+  return {
+    getInstance: function() {
+
+      // Public method to get the single instance
+      if (!instance) {
+        instance = createInstance();
+      }
+      return instance;
+    }
+  };
+})();
+
+const saveToPdf = async (htmlContent) => {
+  const startTime = performance.now();
   // Browser actions & buffer creator
-  const browser = await puppeteer.launch({
-    executablePath: "/usr/bin/chromium-browser",
-    args: ["--no-sandbox", "--disable-setuid-sandbox", "--run-all-compositor-stages-before-draw"] // SEE BELOW WARNING!!!
-  });
+  const browser = await browserHelper.getInstance();
   const page = await browser.newPage();
   await page.setContent(htmlContent, {
-    waitUntil: ["networkidle0", "load"],
+    waitUntil: ["load"],
   });
   const pdf = await page.pdf({
     printBackground: true,
     format: 'A4'
   });
-  await browser.close();
+  await page.close();
+
+  // duration
+  const endTime = performance.now();
+  const duration = endTime - startTime;
+  console.log("saveToPdf duration: " + duration + " milliseconds");
+
   // Return Buffer
   return pdf;
 };
